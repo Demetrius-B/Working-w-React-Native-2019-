6d9ce768cd65a9a24be30d9d3181a023a72ba3d0
demetrius-b <Dberkeley@fullsail.edu>
2019-08-24 16:26:03 -0400

    added keys to each friend

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,12 +3,12 @@ import { Text, View, StyleSheet, FlatList } from 'react-native'
 
 const ListScreen = () => {
     const friends = [
-        {name: 'Demetrius'},
-        {name: 'Ryan'},
-        {name: 'Brandon'},
-        {name: 'Tyler'},
-        {name: 'Gary'},
-        {name: 'Adue'},
+        {name: 'Demetrius', key:'1'},
+        {name: 'Ryan', key:'2'},
+        {name: 'Brandon', key:'3'},
+        {name: 'Tyler', key:'4'},
+        {name: 'Gary', key:'5'},
+        {name: 'Adue', key:'6'},
     ]
 
     return (
