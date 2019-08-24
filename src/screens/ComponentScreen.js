ea15d4421621ac48a771ae2d572426858d4054bd
demetrius-b <Dberkeley@fullsail.edu>
2019-08-24 14:25:09 -0400

    expanded the return statement."

--- src/screens/ComponentScreen.js
+++ src/screens/ComponentScreen.js
@@ -1,8 +1,12 @@
 import React from 'react'
-import { Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
-const ComponentScreen =  () => {
-    return <Text style={styles.textStyle}>This is the components screen.</Text>
+const ComponentScreen = () => {
+    return (
+        <View>
+            <Text style={styles.textStyle}>This is the components screen.</Text>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({ // CSS in React Native
