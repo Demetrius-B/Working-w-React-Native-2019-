de0059960999878089f603956a79593d272b80c9
demetrius-b <Dberkeley@fullsail.edu>
2019-08-24 09:23:54 -0400

    created a return for componentsScreen & added stylesheet

---
+++ src/screens/ComponentScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, StyleSheet } from 'react-native'
+
+const ComponentScreen =  () => {
+    return <Text>This is the components screen.</Text>
+}
+
+const styles = StyleSheet.create({ // CSS in React Native
+    textStyle: {
+
+    }
+})
