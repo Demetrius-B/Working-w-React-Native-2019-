0af087e007f879a40ba7160274c6daaac388962b
demetrius-b <Dberkeley@fullsail.edu>
2019-08-24 16:33:01 -0400

    used 2 methods to define a key

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -2,20 +2,33 @@ import React from 'react'
 import { Text, View, StyleSheet, FlatList } from 'react-native'
 
 const ListScreen = () => {
+    // Method #1 - Defining a key
+    // const friends = [
+    //     {name: 'Demetrius', key:'1'},
+    //     {name: 'Ryan', key:'2'},
+    //     {name: 'Brandon', key:'3'},
+    //     {name: 'Tyler', key:'4'},
+    //     {name: 'Gary', key:'5'},
+    //     {name: 'Adue', key:'6'},
+    // ]
+
+
+    // Method #1 - Defining a key
     const friends = [
-        {name: 'Demetrius', key:'1'},
-        {name: 'Ryan', key:'2'},
-        {name: 'Brandon', key:'3'},
-        {name: 'Tyler', key:'4'},
-        {name: 'Gary', key:'5'},
-        {name: 'Adue', key:'6'},
+        {name: 'Demetrius'},
+        {name: 'Ryan'},
+        {name: 'Brandon'},
+        {name: 'Tyler'},
+        {name: 'Gary'},
+        {name: 'Adue'},
     ]
 
     return (
         <FlatList 
+            keyExtractor={(friend) => friend.name}
             data={friends} 
             renderItem={({item}) => {
-                return <Text>{item.name}</Text>
+                return  <Text>{item.name}</Text>
             }} 
         />
     )
