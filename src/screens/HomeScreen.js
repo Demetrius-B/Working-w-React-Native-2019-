9ee260ef2aa2105ceb198ac00979fa1da74b1bbd
demetrius-b <Dberkeley@fullsail.edu>
2019-08-25 17:43:56 -0400

    added ImageScreen to navigation

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -12,6 +12,10 @@ const HomeScreen = ({ navigation }) => {
         <Button 
             onPress={() => navigation.navigate('List')}
             title="Go to List Demo" />
+
+        <Button 
+            onPress={() => navigation.navigate('Image')}
+            title="Go to Image Demo" />
     </View>
   )
 };
