e747f6822ed4d743bea27e3950c2ea9bf57486c6
demetrius-b <Dberkeley@fullsail.edu>
2019-08-25 17:43:08 -0400

    added new components ImageCard & Screen

---
+++ src/screens/ImageCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const ImageCard = () => {
+    return (
+        <View>
+            <Text>Image #1</Text>
+        </View>
+    )
+}
+
+export default ImageCard
---
+++ src/screens/ImageScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+
+const ImageScreen = () => {
+    return <View><Text>Need 3 Images</Text></View>
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30
+  }
+});
+
+export default ImageScreen;
