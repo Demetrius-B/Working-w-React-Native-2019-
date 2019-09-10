f6317d7796275482b741fb8e110085b6d9a33023
demetrius-b <Dberkeley@fullsail.edu>
2019-09-10 17:34:55 -0400

    created a reducer that changes state for red, green, blue, and returning a new object to use for state

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react'
+import React, {useReducer} from 'react'
 import {View, Text, StyleSheet} from 'react-native'
 import ColorCounter from '../components/ColorCounter'
 
@@ -8,30 +8,48 @@ const COLOR_INCREMENT = 15;
 
 const SquareScreen = () => {
     const reducer = (state, action) => {
-    // state === { red: number, green: number, blue: number}
-    // action: an object that tells me how to change my state object.
-    // action === { colorToChange: 'red || 'blue' || 'green', amount: 15 || -15}
+        // state === { red: number, green: number, blue: number} action: an object that
+        // tells me how to change my state object. action === { colorToChange: 'red ||
+        // 'blue' || 'green', amount: 15 || -15}
 
+        switch (action.colorToChange) {
+            case 'red':
+                // NEVER DO THIS: state.red = state.red - 15 Rebuild state object into new
+                // object w/ changed value Make new object {Copy everything from old state,
+                // Overwrite new red state prop}
+                return {
+                    ...state,
+                    red: state.red + action.amount
+                }
+            case 'green':
+                return {
+                    ...state,
+                    green: state.green + action.amount
+                }
+            case 'blue':
+                return {
+                    ...state,
+                    blue: state.blue + action.amount
+                }
+            default:
+                return state;
+        }
     }
 
-    const [state, dispatch] = useReducer(reducer, {red: 0, green: 0, blue: 0});
+    const [state,
+        dispatch] = useReducer(reducer, {
+        red: 0,
+        green: 0,
+        blue: 0
+    });
 
     return (
         <View>
-            <ColorCounter
-                onIncrease={() => }
-                onDecrease={() => }
-                color="red"/>
+            <ColorCounter onIncrease={() =>} onDecrease={() =>} color="red"/>
 
-            <ColorCounter
-                onIncrease={() => }
-                onDecrease={() => }
-                color="green"/>
+            <ColorCounter onIncrease={() =>} onDecrease={() =>} color="green"/>
 
-            <ColorCounter
-                onIncrease={() => }
-                onDecrease={() => }
-                color="blue"/>
+            <ColorCounter onIncrease={() =>} onDecrease={() =>} color="blue"/>
 
             <View
                 style={{
