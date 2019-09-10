579541fb756fea75e678eac08760f03f336e036e
demetrius-b <Dberkeley@fullsail.edu>
2019-09-10 17:44:38 -0400

    reducer fuctionality wired up to onIncrease & decrease for ColorCounter

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -5,36 +5,36 @@ import ColorCounter from '../components/ColorCounter'
 // useReducer - Hook, Add some functionality to a component.
 
 const COLOR_INCREMENT = 15;
+const reducer = (state, action) => {
+    // state === { red: number, green: number, blue: number} action: an object that
+    // tells me how to change my state object. action === { colorToChange: 'red ||
+    // 'blue' || 'green', amount: 15 || -15}
+
+    switch (action.colorToChange) {
+        case 'red':
+            // NEVER DO THIS: state.red = state.red - 15 Rebuild state object into new
+            // object w/ changed value Make new object {Copy everything from old state,
+            // Overwrite new red state prop}
+            return {
+                ...state,
+                red: state.red + action.amount
+            }
+        case 'green':
+            return {
+                ...state,
+                green: state.green + action.amount
+            }
+        case 'blue':
+            return {
+                ...state,
+                blue: state.blue + action.amount
+            }
+        default:
+            return state;
+    }
+}
 
 const SquareScreen = () => {
-    const reducer = (state, action) => {
-        // state === { red: number, green: number, blue: number} action: an object that
-        // tells me how to change my state object. action === { colorToChange: 'red ||
-        // 'blue' || 'green', amount: 15 || -15}
-
-        switch (action.colorToChange) {
-            case 'red':
-                // NEVER DO THIS: state.red = state.red - 15 Rebuild state object into new
-                // object w/ changed value Make new object {Copy everything from old state,
-                // Overwrite new red state prop}
-                return {
-                    ...state,
-                    red: state.red + action.amount
-                }
-            case 'green':
-                return {
-                    ...state,
-                    green: state.green + action.amount
-                }
-            case 'blue':
-                return {
-                    ...state,
-                    blue: state.blue + action.amount
-                }
-            default:
-                return state;
-        }
-    }
 
     const [state,
         dispatch] = useReducer(reducer, {
@@ -42,14 +42,35 @@ const SquareScreen = () => {
         green: 0,
         blue: 0
     });
+    const {red, green, blue} = state;
+
+    // dispatch === runMyReducer
 
     return (
         <View>
-            <ColorCounter onIncrease={() =>} onDecrease={() =>} color="red"/>
+            <ColorCounter
+                onIncrease={() => dispatch({colorToChange: 'red', amount: COLOR_INCREMENT})}
+                onDecrease={() => dispatch({
+                colorToChange: 'red',
+                amount: -1 * COLOR_INCREMENT
+            })}
+                color="red"/>
 
-            <ColorCounter onIncrease={() =>} onDecrease={() =>} color="green"/>
+            <ColorCounter
+                onIncrease={() => dispatch({colorToChange: 'green', amount: COLOR_INCREMENT})}
+                onDecrease={() => dispatch({
+                colorToChange: 'green',
+                amount: -1 * COLOR_INCREMENT
+            })}
+                color="green"/>
 
-            <ColorCounter onIncrease={() =>} onDecrease={() =>} color="blue"/>
+            <ColorCounter
+                onIncrease={() => dispatch({colorToChange: 'blue', amount: COLOR_INCREMENT})}
+                onDecrease={() => dispatch({
+                colorToChange: 'blue',
+                amount: -1 * COLOR_INCREMENT
+            })}
+                color="blue"/>
 
             <View
                 style={{
