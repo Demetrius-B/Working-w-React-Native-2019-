d965c85c508171052af51cab8635f7338a2c0efc
demetrius-b <Dberkeley@fullsail.edu>
2019-09-13 14:30:51 -0400

    added 3 boxes to prative styling

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,19 +3,40 @@ import {Text, View, StyleSheet} from 'react-native'
 
 const BoxScreen = () => {
     return <View style={styles.viewStyle}>
-        <Text style={styles.textStyle}>Boc Screen</Text>
+        <Text style={styles.textOneStyle}>1</Text>
+        <Text style={styles.textTwoStyle}>2</Text>
+        <Text style={styles.textThreeStyle}>3</Text>
     </View>
 }
 
 const styles = StyleSheet.create({
     viewStyle: {
         borderWidth: 1,
-        borderColor: 'black'
+        borderColor: 'black',
+        height: 200,
+        flexDirection: "row",
+        justifyContent: "space-between"
     },
-    textStyle: {
+    textOneStyle: {
         borderWidth: 1,
         borderColor: 'red',
-        margin: 20
+        backgroundColor: 'red',
+        height: 50,
+        width: 50
+    },
+    textTwoStyle: {
+        borderWidth: 1,
+        borderColor: 'green',
+        backgroundColor: 'green',
+        height: 50,
+        width: 50,
+    },
+    textThreeStyle: {
+        borderWidth: 1,
+        borderColor: 'blue',
+        backgroundColor: 'blue',
+        height: 50,
+        width: 50
     }
 })
 
